1b6bc196b4dd09abb38c54875c883a0ce37d301b
Thato Madubanya <madub.thato@gmail.com>
2021-10-27 00:42:39 +0200

    Jwt aut created

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,7 +1,51 @@
+using System.Threading.Tasks;
+using dotnetRpg.Data;
+using dotnetRpg.Dtos.Character.User;
+using dotnetRpg.Dtos.User;
+using dotnetRpg.Models;
+using Microsoft.AspNetCore.Mvc;
+
 namespace dotnetRpg.Controllers
 {
-    public class AuthController
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
     {
-        
+        private readonly IAuthRepository _authRepo;
+        public AuthController(IAuthRepository authRepo)
+        {
+            _authRepo = authRepo;
+
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDto request)
+        {
+            var response = await _authRepo.Register(
+                new User { Username = request.Username },request.Password
+            );
+
+            if(!response.Success)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<ServiceResponse<string>>> Login(UserLoginDto request)
+        {
+            var response = await _authRepo.Login(
+                request.Username ,request.Password
+            );
+
+            if(!response.Success)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
     }
 }
--- Data/IAuthRepository.cs
+++ Data/IAuthRepository.cs
@@ -3,7 +3,7 @@ using dotnetRpg.Models;
 
 namespace dotnetRpg.Data
 {
-    public class IAuthRepository
+    public interface IAuthRepository
     {
         Task<ServiceResponse<int>> Register(User user,string password);
         Task<ServiceResponse<string>> Login(string username,string password);
